f67f2edd4748207773d1bd2ea026dea6938f356c
David Edmundson <kde@davidedmundson.co.uk>
2011-11-21 23:41:48 +0000

    Call checkForUpdate() when global presence changes.
    Reviewed by: Martin Klapetek

--- global/globalpresencesource.cpp
+++ global/globalpresencesource.cpp
@@ -75,6 +75,7 @@ void GlobalPresenceSource::onCurrentPresenceChanged(Tp::Presence newPresence)
     }
 
     setData("presenceMessage", newPresence.statusMessage());
+    checkForUpdate();
 }
 
 void GlobalPresenceSource::setGlobalPresenceAccountManager(const Tp::AccountManagerPtr& accountMgr)
